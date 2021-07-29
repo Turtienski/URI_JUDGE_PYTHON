6dceae9e46a82ba47d7f583f8508275336569d19
mac01 <renatoturtienskipossa@gmail.com>
2021-07-29 04:35:07 -0300

    fim

---
+++ Notas_da_Prova.py
@@ -0,0 +1,17 @@
+
+
+def convert(nota_str):
+    nota = int(nota_str)
+    if nota == 0:
+         return 'E'
+    elif nota > 0 and nota < 36:
+        return 'D'
+    elif nota > 35 and nota < 61:
+        return 'C'
+    elif nota > 60 and nota < 85:
+        return 'B'
+    else:
+        return 'A'
+
+entrada = input ()
+print(convert(entrada))
