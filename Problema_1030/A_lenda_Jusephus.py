a4f64c58f64c9883eeb968bc0c05776bb41afc64
mac01 <renatoturtienskipossa@gmail.com>
2021-08-29 01:16:02 -0300

    mais uma tentativa

--- Problema_1030/A_lenda_Jusephus.py
+++ Problema_1030/A_lenda_Jusephus.py
@@ -2,12 +2,19 @@ N = int(input())
 
 for case in range(N):
     n, k = [int(i) for i in input().split()]
-    r = [0]*n
-    for i in range(len(r)):
-        r[i] = i+1
-    j = 0
-    while len(r) != 1:
-        while j < len(r):
-            j = j + k 
-
-    print("Case {}: {}".format(case+1, r[0]))    
+    r = [1]*n
+    proximo = k - 1
+    print(r)
+    for i in range(n-1):
+        r[proximo] = 0 
+        print(r)
+        for j in range(k):
+            print(j, proximo)
+            proximo = proximo + 1
+            if proximo >= n:
+                proximo = proximo - n 
+            try:
+                while r[proximo] == 0:
+                    proximo = proximo + 1  
+            except:
+                print("Case {}: {}".format(case+1, r.index(1)+1))
